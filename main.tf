eb11a111fe09a07acff1be4492529e1c12adf041
trevor.fursa <trevor.fursa@gmail.com>
2024-10-23 18:49:14 +0200

    UbuServer

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.16"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "ubuntu" {
+  name = "ubuntu:20.04"
+}
+
+resource "docker_container" "ubuntu_ansible" {
+  image = docker_image.ubuntu.image_id
+  name  = "ubuntu_ansible_container"
+  ports {
+    internal = 22
+    external = 2222
+  }
+  tty = true
+  stdin_open = true
+
+  provisioner "local-exec" {
+    command = "sleep 10 && ansible-playbook -i 'localhost:2222,' -u root --private-key ~/.ssh/id_rsa playbook.yml"
+  }
+}
+
